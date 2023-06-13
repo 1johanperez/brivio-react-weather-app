a8f3691893aa791918986eb12c34be4ee5b213ec
1johanperez <1johanperez@gmail.com>
2023-06-13 17:20:40 -0400

    sixth commit - first draft of  weather container and weather card mockup create along with mock data for summary view display

--- weather-app/src/components/WeatherCard.jsx
+++ weather-app/src/components/WeatherCard.jsx
@@ -1,3 +1,26 @@
-const WeatherCardComponent = () => <div>WEATHER CARDD COMPONENT</div>;
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
+
+const WeatherCardComponent = (city) => {
+	console.log('city', city);
+
+	return (
+		<Card sx={{ maxWidth: 345 }}>
+			<CardMedia sx={{ height: 140 }} image='/static/images/cards/contemplative-reptile.jpg' title='green iguana' />
+			<CardContent>
+				<Typography gutterBottom variant='h5' component='div'>
+					{city.name}
+				</Typography>
+				<Typography variant='body2' color='text.secondary'>
+					Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents
+					except Antarctica
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Button size='small'>Share</Button>
+				<Button size='small'>Learn More</Button>
+			</CardActions>
+		</Card>
+	);
+};
 
 export default WeatherCardComponent;
--- weather-app/src/components/WeatherContainer.jsx
+++ weather-app/src/components/WeatherContainer.jsx
@@ -1,3 +1,20 @@
-const WeatherContainerComponent = () => <div>WEATHER Container COMPONENT</div>;
+/* eslint-disable react/prop-types */
+import WeatherCardComponent from './WeatherCard';
+
+import Container from '@mui/material/Container';
+const WeatherContainerComponent = ({ cities }) => {
+	console.log('cities', cities);
+
+	return (
+		<>
+			asdfasf
+			<Container style={{ backgroundColor: 'yellow', margin: 'auto' }} maxWidth='sm'>
+				{cities.map((city, index) => (
+					<WeatherCardComponent city={city} key={index} />
+				))}
+			</Container>
+		</>
+	);
+};
 
 export default WeatherContainerComponent;
